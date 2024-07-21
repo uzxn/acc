bec1593746d6b0294c3fed680bc8f103c6cecf15
uzxn <hzxhub@qq.com>
2024-07-21 15:32:16 +0800

    Add files via upload

---
+++ acc.js
@@ -0,0 +1,64 @@
+let data = {};
+data["m3-0"] = { num: 4, song: ["生きとし生けるもの (Ikitoshi Ikerumono) // Promiii", "Rambling Pleat // boomushroom", "一梦千霄 (Yi Meng Qian Xiao) // DongShaoZhou", "White Eternity // tkdkendo"], note: [492, 529, 595, 681] };
+data["m3-1"] = { num: 4, song: ["尘世闲游 (Rex Incognito) // YuzukiY", "恋せよ乙女！ (Koiseyo Otome!) // tkdkendo", "Break // XingRen", "KING // DongShaoZhou"], note: [695, 621, 718, 1279] };
+data["m3-2"] = { num: 4, song: ["Stargazer // Mipha-", "Seyana. (何でも言うことを聞いてくれアカネチャン) // boomushroom", "Love Emotion! // Yashiro-", "Mermaid Girl // Ts27"], note: [1397, 1090, 805, 1212] };
+data["m3-3"] = { num: 4, song: ["恋ひ恋ふ縁 (Koi Kou Enishi) // DongShaoZhou", "お願い！コンコンお稲荷さま (Onegai! Kon on Oinari-sama) // Endermeng", "The Last Page // Oekakizuki vs. Hylotl", "星が降らない街 (Hoshi ga Furanai Machi) // Hylotl"], note: [1055, 1489, 1288, 1789] };
+data["m3-4"] = { num: 4, song: ["ウミユリ海底譚 (Umiyuri Kaiteitan) // MACKEREL", "Fire in the sky // XingRen", "Icyxis // tyrcs", "绯红帝国 (The Crimson Empire) (1.1x) // Oekakizuki"], note: [1865, 1434, 1284, 1839] };
+data["m3-5"] = { num: 4, song: ["白金デスコ (Platinum Disco) // mas3", "キュートなカノジョ (Cute na Kanojo) // tyrcs", "Reimei Sketchbook // Benson_", "Joker // HarKIn"], note: [1282, 1706, 1473, 1939] };
+data["m3-6"] = { num: 4, song: ["チョコレイト・ディスコ (Chocolate Disco) // Oekakizuki", "Call Me, Beep Me (If You Wanna Reap Me) // DongShaoZhou", "DIE IN THE SEA // XingRen", "unhappy century // tera"], note: [1694, 1636, 1803, 2115] };
+data["m3-7"] = { num: 4, song: ["Don't Stop The Music // 7912", "Six Acid Strings // Mipha-", "∀rkadia [Illusion] (1.05x) // Oekakizuki", "Valkyrie Revolutia // OxalicAcid vs. Hylotl"], note: [1701, 1799, 2132, 1899] };
+data["m3-8"] = { num: 4, song: ["Don’t let you down // OxalicAcid vs. crazyImmortals", "夜に駆ける (Yoru ni Kakeru) (1.05x) // tera", "Snow Veil -少女と獣の森- // MACKEREL", "ポジティブ☆ダンスタイム (Positive Dance Time) // Hinaka_Yuki"], note: [2237, 2081, 2280, 2000] };
+data["m3-9"] = { num: 4, song: ["機械少女幻想 (Kikai Shoujo Gensou) // Hylotl", "cold planet // Hinaka_Yuki", "stray star // Sorakaru_", "Unleashed World // Amekaze"], note: [2374, 1889, 2142, 1810] };
+data["m3-10"] = { num: 4, song: ["飛燕草 (Hiensou) // Hinaka_Yuki", "Rocky Buinne // teradora", "月哀·月夜 (Yue Ai Yue Ye) // YuzakiNASB", "Spin Eternally // Oekakizuki"], note: [2034, 1740, 2270, 2166] };
+data["m3-e1"] = { num: 4, song: ["Scorpion Dance // XingRen", "大輪の魂 (Tailin no Soul) (1.2x) // Oekakizuki", "永夜のパレード (Eiya no Parade) (1.05x) // Houraisan Kaguya", "音速宇宙旅行 (Onsoku Uchuu Ryokou) // tera"], note: [1952, 2013, 1953, 2111] };
+data["m3-e2"] = { num: 4, song: ["Moments // Amekaze", "Towards the Horizon // Hinaka_Yuki", "トリカゴ (Torikago) // Hylotl vs. OxalicAcid", "フロンティア↑↑エクスプローラー (Frontier Explorer) (1.15x) // tera"], note: [2107, 1953, 2386, 2674] };
+data["m3-e3"] = { num: 4, song: ["Edison (1.15x) // Hinaka_Yuki", "INTERNET OVERDOSE (1.05x) // Hylotl", "Euthanasia (1.05x) // tera", "Fin.ArcDeaR // Amekaze"], note: [2518, 2636, 2326, 2511] };
+data["m3-e4"] = { num: 4, song: ["ZENITHALIZE // Hinaka_Yuki", "Asymmetrical Grooves // tera", "Pure Ruby // Hylotl", "EVERLASTING HAPPiNESS↑ // crazyImmortals"], note: [2634, 2212, 2336, 2602] };
+data["m3-e5"] = { num: 4, song: ["Bring Our Ignition Back (1.1x) // Oekakizuki", "雲散霧消 // hinako1804", "えれくとりっく・えんじぇぅ (Electric Angel) (1.2x) // Sorakaru_ vs. Hinaka_Yuki", "穢れなき薔薇十字 (Kegare Naki Bara Juuji) // MintApril_"], note: [2734, 2417, 3089, 2974] };
+data["m3-e6"] = { num: 4, song: ["Defeat awaken battle ship // Hylotl", "Extraction // Oekakizuki", "Pastel Subliminal // OxalicAcid", "Synthesized Fortress // tera"], note: [2483, 2276, 2921, 3194] };
+data["m3-e7"] = { num: 4, song: ["LiFE Garden (1.05x) // YuzakiNASB", "Alpha // Zetsfy", "Stay Alive (Zekk Remix) (1.25x) // Hylotl", "Heaven's Fall (1.4x) // tera"], note: [2846, 2260, 2333, 3347] };
+data["m3-e8"] = { num: 4, song: ["はやぶさ (Hayabusa) // Hylotl", "驟雨の狹間 (Shuu no Hazama) // tera", "アマツカミ (Amatsukami) (1.15x) // Hinaka_Yuki", "CRIMSON FIGHTER (1.15x) // OxalicAcid"], note: [3789, 3663, 2424, 3255] };
+data["m3-e9"] = { num: 4, song: ["crazy_tek (DJ Noriken Remix) // tera", "Nhelv // tera", "終点 // Hinaka_Yuki", "Deadly force - Put an end // Sorakaru_ vs. Hinaka_Yuki"], note: [3888, 3030, 3581, 3700] };
+data["m3-ef"] = { num: 4, song: ["Infinity Heaven // tera", "NEURO-CLOUD-9 // tera", "キズアト (Kizuato) (1.3x) // tera", "Runengon // tera"], note: [2828, 3362, 3393, 5100] };
+
+function getRadio() {
+  let choices = document.getElementsByName("dan");
+  for (let i = 0; i < choices.length; i++)
+    if (choices[i].checked == true) return choices[i].value;
+  return false;
+}
+
+function showInput() {
+  let choice = getRadio();
+  if (choice === false || !(choice in data)) {
+    document.getElementById("info").innerHTML = "";
+    document.getElementById("input").innerHTML = "";
+    document.getElementById("result").innerHTML = "还未选择任何段位！/ 该段位没有数据！作者咕了";
+    return;
+  }
+  let info = "曲目信息：<pre>";
+  for (let i = 0; i < data[choice].num; i++) info += `\n${data[choice].song[i]}`;
+  info += "</pre>";
+  let input = "请输入：<br>";
+  for (let j = 1; j <= data[choice].num; j++)
+    input += `<input class="card" type="text" id="input-${j}" placeholder="第 ${j} 首歌结束时的 ACC">\n`;
+  input += `<br><button class="btn primary" onclick="calc()">确定</button>`;
+  document.getElementById("info").innerHTML = info;
+  document.getElementById("input").innerHTML = input;
+  document.getElementById("result").innerHTML = "";
+}
+
+function calc() {
+  let choice = getRadio(), noteNum = data[choice].note;
+  let preNoteNum = [0]; // 物量的前缀和
+  for (let i = 0; i < data[choice].num; i++) preNoteNum[i + 1] = preNoteNum[i] + noteNum[i];
+  let inputAcc = [0]; // inputAcc[1] 为第1首歌结束时的ACC
+  for (let i = 1; i <= data[choice].num; i++)
+    inputAcc[i] = Number(document.getElementById(`input-${i}`).value);
+  let result = "单曲";
+  for (let i = 1; i <= data[choice].num; i++) {
+    let acc = (inputAcc[i] * preNoteNum[i] - inputAcc[i - 1] * preNoteNum[i - 1]) / noteNum[i - 1];
+    result += ` ${acc.toFixed(4)}%`;
+  }
+  document.getElementById("result").innerHTML = result;
+}
